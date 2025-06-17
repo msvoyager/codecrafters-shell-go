1bb4d96c3b52c2985b6cacf0ab25bf623c1d3fa5
MS05-Voyager <minudasaswitha05@gmail.com>
2025-06-17 12:51:28 +0530

    pwd v1 and code refactored

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ var Builtin = map[string]bool{
 	"echo": true,
 	"exit": true,
 	"type": true,
-	
+	"pwd" : true,
 }
 func main() {
 
@@ -32,22 +32,31 @@ func main() {
 			panic(err)
 		}
 
-		trimmedCommand := command[:len(command)-1]
+		trimmedCommand := strings.TrimSpace(command)
+
+		if trimmedCommand == ""{
+			continue
+		} 
 
 		args := strings.Split(trimmedCommand, " ")
-		if len(args) > 1 {
-			switch args[0] {
+
+		switch args[0] {
 			case "exit" :
 			if len(args) == 2 && args[1] == "0" {
 				os.Exit(0)
 			} 
 			case "echo" :
 				fmt.Println(strings.Join(args[1:], " "))
-		
+
 			case "type" :
 				isCommand(args[1])
-				
 
+			case "pwd" :
+				dir, err := os.Getwd()
+				if err != nil {
+					fmt.Println("Error getting current directory:", err)
+				}
+				fmt.Println(dir)
 			default:
 
 				_,executable := isExe(args[0])
@@ -56,17 +65,18 @@ func main() {
 					cmd.Stdout = os.Stdout
 					cmd.Stderr = os.Stderr
 					cmd.Stdin = os.Stdin
-					
+
 					err := cmd.Run()
 					if err != nil {
-						fmt.Println(args[0] + ": command not found")
+						panic(err)
+
 					}
+					continue
+
 				}
-				
+				fmt.Println(args[0] + ": command not found")
+
 			}
-		} else if len(args) == 1 {
-			fmt.Println(args[0] + ": command not found")
-		}
 
 
 	}
@@ -76,8 +86,8 @@ func isCommand(comnd string) {
 	if Builtin[comnd] {
 		fmt.Println(comnd, "is a shell builtin")
 	} else{
-		path,exist:= isExe(comnd, )
-		if exist {
+		path,_:= isExe(comnd)
+		if  path != "" {
 			fmt.Printf("%s is %s\n", comnd, path)
 		} else {
 			fmt.Println(comnd + ": not found")
@@ -94,9 +104,11 @@ func isExe(cmd string) (string, bool) {
 		info,err := os.Stat(fullPath)
 		if err == nil {
 			if !info.IsDir() && info.Mode()&0111 != 0 {
-				return "", true
+				return fullPath, true
+			} else {
+				return fullPath, false
 			}
-			return fullPath, true
+			
 		}		
 	}
 
